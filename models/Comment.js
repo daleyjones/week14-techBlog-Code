a11c11d83253522c808c9ff193b32a30562314aa
daleyjones <djones_15@hotmail.com>
2023-07-15 17:54:14 +0100

    added id, body, post_id

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,33 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 class Comment extends Model {}
 
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     body: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
   },
   {
-    sequelize
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
   }
 );
 
