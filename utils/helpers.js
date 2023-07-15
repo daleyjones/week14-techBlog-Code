a6fa7522cc1c5ab813ba1568be0411982646a3cb
daleyjones <djones_15@hotmail.com>
2023-07-15 16:47:46 +0100

    added format date to helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,16 @@
-module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+  module.exports = {
+    format_date: (date) => {
+      const formattedDate = new Date(date).toLocaleDateString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+      });
+      return formattedDate;
+    },
+    format_amount: (amount) => {
+      // format large numbers with commas
+      return parseInt(amount).toLocaleString();
     }
-  },
-};
+  };
+  
