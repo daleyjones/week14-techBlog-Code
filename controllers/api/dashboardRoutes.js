28d194ee66efbf4baa1ef14990aa380f4b429e13
daleyjones <djones_15@hotmail.com>
2023-07-19 20:24:30 +0100

    added get requests

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -20,3 +20,31 @@ router.get('/', withAuth, async (req, res) => {
     res.redirect('login');
   }
 });
+
+router.get('/new', withAuth, (req, res) => {
+    res.render('new-post', {
+      layout: 'dashboard',
+    });
+  });
+  
+  router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id);
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('edit-post', {
+          layout: 'dashboard',
+          post,
+        });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.redirect('login');
+    }
+  });
+  
+  module.exports = router;
+  
