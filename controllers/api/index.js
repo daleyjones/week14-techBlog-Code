4e6d044d2371163f9c689d2687926f854b085ceb
daleyjones <djones_15@hotmail.com>
2023-07-19 20:57:45 +0100

    moved dashboardroutes out of api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-Routes.js');
+const postRoutes = require('./post-Routes.js');
+const commentRoutes = require('./comment-Routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
