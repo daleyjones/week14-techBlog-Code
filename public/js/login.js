a0e50795a84906488d5b246a2f8b2af96a33dba1
daleyjones <djones_15@hotmail.com>
2023-09-05 20:07:19 +0100

    changed login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,22 @@
 const loginFormHandler = async function(event) {
   event.preventDefault();
 
-  // Collect values from the login form
-  const usernameel = document.querySelector('#username-input-login')
-  const passwordel = document.querySelector('#password-input-login')
+  const usernameEl = document.querySelector('#username-input-login');
+  const passwordEl = document.querySelector('#password-input-login');
 
   const response = await fetch('/api/user/login', {
     method: 'POST',
-    body: JSON.stringify({ 
-      username: usernameel.value,
-      password: passwordel.value,
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
     }),
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    // If successful, redirect the browser to the profile page
     document.location.replace('/dashboard');
   } else {
-    alert(response.statusText);
+    alert('Failed to login');
   }
 };
 
