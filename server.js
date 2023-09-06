1b302056d8a3a9a081f3f3cef8d289ea7e2dd2a2
daleyjones <djones_15@hotmail.com>
2023-09-06 20:29:45 +0100

    added path to config/connection.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection.js');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
