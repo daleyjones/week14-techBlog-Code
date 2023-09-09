14d047c0be6564b33a6a0b608ee881d37677a30e
daleyjones <djones_15@hotmail.com>
2023-09-09 18:06:18 +0100

    fixed syntax

--- public/js/editcomment.js
+++ public/js/editcomment.js
@@ -1,37 +1,35 @@
-const commentId = document.querySelector('input[name="comment-id"]').value;
+const commentId = document.querySelector('input[name="comment-id"]');
 
-const editFormHandler = async function(event) {
+const editFormHandler = async function (event) {
   event.preventDefault();
 
   const body = document.querySelector('textarea[name="comment-body"]').value;
 
   await fetch(`/api/comment/${commentId}`, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify({
       postId,
-      body
+      body,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
-  document.location.replace('/dashboard');
+  document.location.replace("/profile");
 };
 
-const deleteClickHandler = async function() {
+const deleteClickHandler = async function () {
   await fetch(`/api/comment/${commentId}`, {
-    method: 'DELETE'
+    method: "DELETE",
   });
 
-  document.location.replace('/dashboard');
+  document.location.replace("/dashboard");
 };
 
 document
-  .querySelector('#edit-comment-form')
-  .addEventListener('submit', editFormHandler);
+  .querySelector("#edit-comment-form")
+  .addEventListener("submit", editFormHandler);
 document
-  .querySelector('#delete-btn')
-  .addEventListener('click', deleteClickHandler);
-
-
+  .querySelector("#delete-btn")
+  .addEventListener("click", deleteClickHandler);
