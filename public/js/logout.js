2ebac93b567ab34aaa5eff43ad2aa22f3235a3fb
daleyjones <djones_15@hotmail.com>
2023-09-09 18:07:27 +0100

    fixed syntax error

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,15 @@
-const logout = async function() {
+const logout = async function () {
   // console.log('clicked');
-  const response = await fetch('/api/user/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  const response = await fetch("/api/user/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace("/");
   } else {
     alert(response.statusText);
   }
 };
 
-document.querySelector('#logout-link').addEventListener('click', logout);
+document.querySelector("#logout-link").addEventListener("click", logout);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,29 +1,28 @@
-const signupFormHandler = async function(event) {
+const signupFormHandler = async function (event) {
   event.preventDefault();
 
+  const usernameel = document.getElementById("username-input-signup");
 
-    const usernameel = document.getElementById('username-input-signup');
+  const passwordel = document.getElementById("password-input-signup");
 
-    const passwordel = document.getElementById('password-input-signup');
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameel.value,
+      password: passwordel.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-    const response = await fetch('/api/user', {
-      method: 'POST',
-      body: JSON.stringify({ 
-        username: usernameel.value,
-        password: passwordel.value,
-       }),
-      headers: { 'Content-Type': 'application/json' },
-    })
+  // Optionally, you can redirect the user to a new page after successful signup
+  if (response.ok) {
+    // If successful, redirect the browser to the profile page
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+};
 
-    // Optionally, you can redirect the user to a new page after successful signup
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  };
-
-  document
-  .querySelector('#signup-form')
-  .addEventListener('submit', signupFormHandler);
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
